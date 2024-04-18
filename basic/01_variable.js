4deaf77e1c791494ad91dcf3f00a8e5cf4b706cf
saurabh mishra <111421281+saurabhsmishraa@users.noreply.github.com>
2024-04-18 08:41:04 +0000

    datatypes

---
+++ basic/01_variable.js
@@ -0,0 +1,37 @@
+const accountId = 197868
+
+let accountEmail = "saurabhpbh1999@gmail.com"
+
+var accountPassword = "23541";
+
+accountCity = "pratapgarh"
+
+//investigation
+
+//const variable cannot be reasssign
+//prefer not to use var because of issue in block and functional scope
+
+
+
+
+ //accountId = 12423  
+ //console.log(accountId);
+
+
+  accountEmail = "saurabhmishra@gmail.com"
+//  console.log(accountEmail);
+
+
+accountPassword = "12345";
+// console.log(accountPassword);
+
+accountCity = "lucknow";
+// console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+
+
+
+let accountState;
+console.log(accountState);
---
+++ basic/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat as all js code as newer version
+
+// alert(3+7)   // alert is working on dom not in nodejs
+
+// number ,
+// bigint,
+// string => '', "",
+// boolean => true/false,
+// null => standalone value // null is a object datatype
+// undefined => let state  // values not assign
+// symbol => unique
+
+// object
+
+let name = "saurabh";
+console.log(typeof name);
+let age = 18;
+let isLoggedIn = true;
