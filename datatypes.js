2b9a6615011a0199084acf6190342c4f3fecd1f6
saurabhsmishraa <saurabhpbh1999@gmail.com>
2024-07-02 16:22:01 +0530

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+"use strict";  // treat all js code as latest js code
+// number =>2 power 53
+// bigint
+// string => ""
+// boolean=>true/false
+// null => standalone value
+// undefined => value not define
+// symbol => unique
+
+//object
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof Object)
