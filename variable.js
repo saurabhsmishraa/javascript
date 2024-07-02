ae10910c606ee57c682e1d75fd29ae4cf662d55b
saurabhsmishraa <saurabhpbh1999@gmail.com>
2024-07-02 15:58:10 +0530

    javascript variable

---
+++ variable.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "saurbhmishra@gmail.com"
+var accountPassword = "12345678"
+accountCity = "jaipur"
+
+// accountId = 2 // not allowed 
+console.log(accountId)
+
+accountEmail = "sm@gmail.com"
+// console.log(accountEmail)
+
+accountPassword = "12345";
+// console.log(accountPassword)
+
+accountCity = "uttar pradesh"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
