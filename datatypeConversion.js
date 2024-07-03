1f321038b7ff77f44e15ce90388b4acd5af65561
saurabhsmishraa <saurabhpbh1999@gmail.com>
2024-07-03 18:58:25 +0530

    datatype conversion practice

---
+++ datatypeConversion.js
@@ -0,0 +1,26 @@
+// let score = "33avw"
+// let score = false
+// let score = "saurabh"
+
+// console.log(typeof score);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+
+let str1 = "hello"
+let str2 = " saurabh"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 3)
+console.log(1 + 2 + "3")
+
+console.log(true)
+console.log(+true);
