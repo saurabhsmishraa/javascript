894de681998e5c988e047ed2942169d6fd952c86
saurabhsmishraa <saurabhpbh1999@gmail.com>
2024-07-20 09:50:20 +0530

    this is string lecture latest

---
+++ string.js
@@ -0,0 +1,39 @@
+// const name = "saurabh"
+// const age = 24
+// console.log(name + age);
+
+// console.log(`hello my name is ${name} and my age ${age}`);
+
+
+// const laptop_name = new String('msi');
+// console.log(laptop_name[0])
+// console.log(laptop_name.__proto__)
+// console.log(laptop_name.length)
+// console.log(laptop_name.toUpperCase());
+// console.log(laptop_name.charAt(1))
+
+// console.log(laptop_name.indexOf('m'))
+
+
+// const gameName = "avenger-infinity"
+// const newString = gameName.substring(0, 4)
+// console.log(newString)
+
+// const anotherString = gameName.slice(0, -2)
+// console.log(anotherString)
+
+// const newStringOne = "  saurabh  mishra   "
+// console.log(newStringOne)
+// console.log(newStringOne.trim());
+
+
+// const url = "https://google.com/google%10yahoo"
+// console.log(url.replace('%10', '-'))
+// console.log(url.includes('yahoo'))
+
+
+
+const firstname = 'saurabh-mishra-up';
+console.log(firstname.split('-'))
+
+
