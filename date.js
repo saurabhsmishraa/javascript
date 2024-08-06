254ab4e62cf11462ebc093bfcf3143176005c694
saurabh mishra <111421281+saurabhsmishraa@users.noreply.github.com>
2024-08-06 15:10:27 +0530

    Add files via upload
    this is date function of javascript

---
+++ date.js
@@ -0,0 +1,30 @@
+let myDate = new Date();
+// console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+
+
+// let myCreateDate = new Date(2024, 7, 6);
+// console.log(myCreateDate.toDateString());
+
+
+// let CreateDate = new Date(2024, 7, 6);
+// console.log(CreateDate.toLocaleString());
+
+let CreateDate = new Date("01-05-1998");
+// console.log(CreateDate.toLocaleDateString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp)
+
+// console.log(CreateDate.getTime());
+
+// console.log(Math.floor((Date.now() / 1000)));
+
+
+let newDate = new Date()
+console.log(newDate.getUTCDay());
