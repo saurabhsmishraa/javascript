00598b0121d8e18fdf835fd8657b581f2a163b83
saurabh mishra <111421281+saurabhsmishraa@users.noreply.github.com>
2024-08-08 12:02:48 +0530

    this is function with object and array

---
+++ function1.js
@@ -0,0 +1,43 @@
+function calculateCartPrice(val1, val2, val3, ...num1) {
+    return num1
+}
+// console.log(calculateCartPrice(100, 200, 300, 400, 500, 600))
+
+
+
+
+
+//function with object 
+
+// const user = {
+//     username: "saurabh mishra",
+//     prices: 199
+// }
+// function handelObject(anyObject) {
+//     console.log(`username is ${anyObject.username} and price is ${anyObject.prices}`)
+
+// }
+// handelObject(user);
+
+
+
+
+
+
+handelObject({
+    username: "saurabh mishra",
+    price: 193
+})
+function handelObject(anyObject) {
+    // console.log(`username is ${anyObject.username} and price is ${anyObject.price}`)
+}
+
+
+// function with array
+const newArray = [100, 200, 300, 400, 500, 600]
+function returnsecondValue(getArray) {
+    return getArray[1]
+}
+// console.log(returnsecondValue(newArray))
+
+console.log(returnsecondValue([111, 3, 112, 113, 114, 115, 116]))
